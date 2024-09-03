6890dc2026b03ea14cfee2ada78b9837ed9e44ed
stefv21 <161451930+stefv21@users.noreply.github.com>
2024-09-03 15:29:49 -0400

    new update adding a loop

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,4 +1,26 @@
-alert('Hello world');
 
-favoriteFood ='popcorn'
-document.write(favoriteFood);
+
+let pokemon = 'pokemonList';
+console.log(typeof pokemon);
+
+
+let pokemonlist = [
+  { name: 'pikachu', type: 'electric', height: '7'},
+  { name: 'charmeleon', type: 'fire', height: '3'},
+  { name: 'pidgeot', type: 'normal', height: '6'}
+];
+
+
+for (let i=0; i < pokemonlist.length; i++){
+    let height = parseFloat (pokemonlist[i].height); 
+    let pokemon = pokemonlist[i];
+
+    if (height < 4) {
+        console.log(`${pokemon.name} (Height: ${pokemon.height}) is small`);
+      } else if (height >=5 && height <=6) {
+        console.log(`${pokemon.name} (Height: ${pokemon.height}) is average`);
+      } else {
+        console.log(`${pokemon.name} (Height: ${pokemon.height}) Wow, that's big!`);
+      }
+    }
+
