aa6b611ea5483302116fce0db5b24e21bd8f0714
stefv21 <161451930+stefv21@users.noreply.github.com>
2024-09-16 19:18:57 -0400

    updated load details

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,10 +1,8 @@
 const pokemonRepository = (function () {
     // Private array to hold Pokémon
-    let pokemonList = [
-        { name: 'pikachu', type: 'electric', height: '7' },
-        { name: 'charmeleon', type: 'fire', height: '3' },
-        { name: 'pidgeot', type: 'normal', height: '6' }
-    ];
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+      
 
     // Function to get all Pokémon
     function getAll() {
@@ -20,12 +18,12 @@ const pokemonRepository = (function () {
         }
     }
 
-    // Function to display Pokémon information
+    
     function displayPokemonInfo() {
         pokemonList.forEach(pokemon => {
             let height = parseFloat(pokemon.height); // Convert height to a number
 
-            // Categorize Pokémon based on height and display result
+            
             if (height < 4) {
                 document.write(`${pokemon.name} (Height: ${pokemon.height}) is small<br>`);
             } else if (height >= 5 && height <= 6) {
@@ -46,26 +44,73 @@ const pokemonRepository = (function () {
         listpokemon.appendChild (button)
         pokemonList.appendChild(listpokemon);
         button.addEventListener('click', function () {
-            console.log(pokemon);
+            showDetails(pokemon);
         });
     }
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+          return response.json();
+        }).then(function (json) {
+          let promises = json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+            return loadDetails(pokemon);
+          });
+          return Promise.all(promises);
+        }).catch(function (e) {
+          console.error(e);
+        })
+      }
     
+    function loadDetails (item) {
+        let url = pokemon.detailsUrl;
+        return fetch(url). then (function (response) {
+            return response.json();
+        }).then (function (details){
+            pokemon.imageUrl = details.sprites.front_default;
+            pokemon.height = details.height;
+            pokemon.types = details.types; 
+        }).catch (function (e) {
+            console.error (e);
+        });
+    }
+
+
         function showDetails(pokemon) {
-            console.log(pokemon);
-        }
+            loadDetails (pokemon).then (function(){
+            console.log(`Name: ${pokemon.name}`);
+            console.log(`Height: ${pokemon.height}`);
+            console.log(`Types: ${pokemon.types}`);
+            console.log(`Image URL: ${pokemon.imageUrl}`);;
+            }).catch(function (e) {
+                console.error('Failed to load Pokémon details:', e);
+            
+       });
+    }
+
         return {
             getAll: getAll,
             add: add,
             displayPokemonInfo: displayPokemonInfo,
             addListItem: addListItem,
-            showDetails: showDetails
+            showDetails: showDetails,
+            loadList:loadList,
+            loadDetails: loadDetails
         };
     })();
 
 
  //Use the public method to get all Pokémon and iterate over them
 
-pokemonRepository.getAll().forEach (function(pokemon){
-pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function(){
+    pokemonRepository.getAll().forEach (function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
+});
+
+
+
 
